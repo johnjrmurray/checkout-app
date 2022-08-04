92dd02bc949035dcb6e81eb81c6bf44d07f0c586
John-Junior Murray <johnjrmurray@users.noreply.github.com>
2022-08-04 23:04:02 +0100

    tidying code and disabling forcing covers annotation

--- src/App.php
+++ src/App.php
@@ -1,163 +1,173 @@
 <?php
-	
-	declare(strict_types=1);
 
-	namespace Checkout;
+declare(strict_types=1);
 
-	class App {
-		
-		public $items;
-  		
-		public function __construct() {
-			$this->items = [];
-  		}
+namespace Checkout;
 
-		// Add an item to the checkout
-		public function addItem($sku, $quantity): bool {
-			if(!isset($this->items[$sku])) {
-				$this->items[$sku] = $quantity;
-			} else {
-				$this->items[$sku] += $quantity;
+class App
+{
 
+	public $items;
+
+	public function __construct()
+	{
+		$this->items = [];
+	}
+
+	// Add an item to the checkout
+	public function addItem($sku, $quantity): bool
+	{
+		if (!isset($this->items[$sku])) {
+			$this->items[$sku] = $quantity;
+		} else {
+			$this->items[$sku] += $quantity;
+		}
+
+		return true;
+	}
+
+	// Remove an item from the checkout
+	public function removeItem($sku, $quantity): bool
+	{
+		if (isset($this->items[$sku])) {
+			$this->items[$sku] -= $quantity;
+		}
+
+		return true;
+	}
+
+	// Get all current items
+	public function getItems(): array
+	{
+		return $this->items;
+	}
+
+	// Get total of all items at checkout
+	public function getTotal(): float
+	{
+
+		$checkoutTotal = 0;
+
+		foreach ($this->items as $item => $quantity) {
+			$checkoutTotal += $this->calculateItemTotal($item, $quantity);
+		}
+
+		return $checkoutTotal;
+	}
+
+	// Calculate total for particular item
+	public function calculateItemTotal($sku, $quantity): float
+	{
+		$itemPrice = $this->getItemPrice($sku);
+		$itemDiscounts = $this->calculateItemDiscounts($sku, $quantity);
+
+		$itemTotal = $itemPrice * $quantity;
+		$itemTotal -= $itemDiscounts;
+
+		return $itemTotal;
+	}
+
+	// Get discount for a particular item
+	public function calculateItemDiscounts($sku, $quantity): float
+	{
+
+		$discountArray = $this->getDiscounts($sku);
+		$itemDiscount = 0;
+
+		// If there are no discounts applicable 
+		if (empty($discountArray)) {
+			null;
+		} elseif (isset($discountArray['eligibleProductPair'])) {
+			// If there is an eligable product pair
+			// For the quantity of that item, then discount by
+			// the specified amount
+
+
+			// Get quantity of product pair
+			$checkoutItems = $this->getItems();
+
+			if (!isset($checkoutItems[$discountArray['eligibleProductPair']])) {
+				return $itemDiscount;
 			}
 
-			return true;
-		}
+			$pairedItems = $checkoutItems[$discountArray['eligibleProductPair']];
 
-		// Remove an item from the checkout
-		public function removeItem($sku, $quantity): bool {
-			if(isset($this->items[$sku])) {
-				$this->items[$sku] -= $quantity;
-			} 
-		}
-
-		// Get all current items
-		public function getItems(): array {
-			return $this->items;
-		}
-
-		// Get total of all items at checkout
-		public function getTotal(): float {
-
-			$checkoutTotal = 0;
-
-			foreach($this->items as $item => $quantity) {
-				$checkoutTotal += $this->calculateItemTotal($item, $quantity);
-			}
-			
-			return $checkoutTotal;
-		}
-
-		// Calculate total for particular item
-		public function calculateItemTotal($sku, $quantity): float {
-			$itemPrice = $this->getItemPrice($sku);
-			$itemDiscounts = $this->calculateItemDiscounts($sku, $quantity);
-
-			$itemTotal = $itemPrice * $quantity;
-			$itemTotal -= $itemDiscounts;
-
-			return $itemTotal;
-		}
-
-		// Get discount for a particular item
-		public function calculateItemDiscounts($sku, $quantity): float {
-
-			$discountArray = $this->getDiscounts($sku);
-			$itemDiscount = 0;
-
-			// If there are no discounts applicable 
-			if(empty($discountArray)) {
-				null;
-			} elseif(isset($discountArray['eligibleProductPair'])) {
-				// If there is an eligable product pair
-				// For the quantity of that item, then discount by
-				// the specified amount
-				
-				
-				// Get quantity of product pair
-				$checkoutItems = $this->getItems();
-				
-				if(!isset($checkoutItems[$discountArray['eligibleProductPair']])) {
-					return $itemDiscount;
+			for ($i = 1; $i <= $quantity; $i++) {
+				if ($i <= $pairedItems) {
+					$itemDiscount += $discountArray['totalDiscount'];
 				}
-
-				$pairedItems = $checkoutItems[$discountArray['eligibleProductPair']];
-				
-				for($i = 1; $i <= $quantity; $i++) {
-					if($i <= $pairedItems) {
-						$itemDiscount += $discountArray['totalDiscount'];
-					}
-				} 
+			}
 			// Multiple discounts available 
-			} elseif(count($discountArray) > 1 && (isset($discountArray[0]) && is_array($discountArray[0]))) {
+		} elseif (count($discountArray) > 1 && (isset($discountArray[0]) && is_array($discountArray[0]))) {
 
-				// Sort array in descending order - highest possible discount will be first
-				$discountArray = array_reverse($discountArray);
+			// Sort array in descending order - highest possible discount will be first
+			$discountArray = array_reverse($discountArray);
 
-				$tempQty = $quantity;
+			$tempQty = $quantity;
 
-				foreach($discountArray as $discount) {
+			foreach ($discountArray as $discount) {
 
-					for($i = 0; $i < $quantity; $i++) {
-						// Attempt to take discount quantity off
-						if($tempQty - $discount['eligibleQuantity'] < 0) {
-							continue;
-						} else {
-							$itemDiscount += $discount['totalDiscount'];
-							$tempQty -= $discount['eligibleQuantity'];
-						}
+				for ($i = 0; $i < $quantity; $i++) {
+					// Attempt to take discount quantity off
+					if ($tempQty - $discount['eligibleQuantity'] < 0) {
+						continue;
+					} else {
+						$itemDiscount += $discount['totalDiscount'];
+						$tempQty -= $discount['eligibleQuantity'];
 					}
 				}
-			} else {
-				// Simple discount - work out how many times to discount the item
-				$discountTimes = floor($quantity / $discountArray['eligibleQuantity']);
-				$itemDiscount = $discountArray['totalDiscount'] * $discountTimes;
 			}
-
-			return $itemDiscount;
-
+		} else {
+			// Simple discount - work out how many times to discount the item
+			$discountTimes = floor($quantity / $discountArray['eligibleQuantity']);
+			$itemDiscount = $discountArray['totalDiscount'] * $discountTimes;
 		}
 
-		// Set up item prices
-		public function getItemPrice($sku): float {
-			$itemPrices = [
-				'A' => 0.50,
-				'B' => 0.30,
-				'C' => 0.20,
-				'D' => 0.15,
-				'E' => 0.05
-			];
+		return $itemDiscount;
+	}
 
-			return $itemPrices[$sku];
-		}
+	// Set up item prices
+	public function getItemPrice($sku): float
+	{
+		$itemPrices = [
+			'A' => 0.50,
+			'B' => 0.30,
+			'C' => 0.20,
+			'D' => 0.15,
+			'E' => 0.05
+		];
 
-		// Get discounts for item
-		public function getDiscounts($sku): array {
-			$discounts = [
-				'A' => [
-					'eligibleQuantity' => 3,
-					'totalDiscount' => 0.20
-				],
-				'B' => [
+		return $itemPrices[$sku];
+	}
+
+	// Get discounts for item
+	public function getDiscounts($sku): array
+	{
+		$discounts = [
+			'A' => [
+				'eligibleQuantity' => 3,
+				'totalDiscount' => 0.20
+			],
+			'B' => [
+				'eligibleQuantity' => 2,
+				'totalDiscount' => 0.15
+			],
+			'C' => [
+				[
 					'eligibleQuantity' => 2,
-					'totalDiscount' => 0.15
-				],
-				'C' => [
-					[
-						'eligibleQuantity' => 2,
-						'totalDiscount' => 0.02
-					], [
-						'eligibleQuantity' => 3,
-						'totalDiscount' => 0.10
-					]
-				],
-				'D' => [
-					'totalDiscount' => 0.10,
-					'eligibleProductPair' => 'A'
-				],
-				'E' => []
-			];
+					'totalDiscount' => 0.02
+				], [
+					'eligibleQuantity' => 3,
+					'totalDiscount' => 0.10
+				]
+			],
+			'D' => [
+				'totalDiscount' => 0.10,
+				'eligibleProductPair' => 'A'
+			],
+			'E' => []
+		];
 
-			return $discounts[$sku];
-		}
+		return $discounts[$sku];
+	}
 }
--- tests/AppTest.php
+++ tests/AppTest.php
@@ -1,113 +1,111 @@
 <?php
-    declare(strict_types=1);
 
-    namespace Checkout; 
-    use PHPUnit\Framework\TestCase;
+declare(strict_types=1);
 
-    class AppTest extends TestCase
-    {
-        /**
-          * @covers \App\CalculatePrice
-          */
-        public function testProductA(): void
-        {
-          $checkout = new App;
+namespace Checkout;
 
-          $checkout->addItem('A', 1);
-          $this->assertEquals('0.50',$checkout->getTotal());
+use PHPUnit\Framework\TestCase;
 
-          $checkout->addItem('A', 2);
-          $this->assertEquals('1.30',$checkout->getTotal());
-        
-          $checkout->addItem('A', 1);
-          $this->assertEquals('1.80',$checkout->getTotal());
+class AppTest extends TestCase
+{
+	public function testProductA(): void
+	{
+		$checkout = new App;
 
-        }
+		// Check standard price
+		$checkout->addItem('A', 1);
+		$this->assertEquals('0.50', $checkout->getTotal());
 
-        /**
-          * @covers \App\CalculatePrice
-          */
-          public function testProductB(): void
-          {
-            $checkout = new App;
-  
-            $checkout->addItem('B', 1);
-            $this->assertEquals('0.30',$checkout->getTotal());
-  
-            $checkout->addItem('B', 1);
-            $this->assertEquals('0.45',$checkout->getTotal());
-          
-            $checkout->addItem('B', 1);
-            $this->assertEquals('0.75',$checkout->getTotal());
-  
-          }
+		// Check discounted price
+		$checkout->addItem('A', 2);
+		$this->assertEquals('1.30', $checkout->getTotal());
 
-          /**
-          * @covers \App\CalculatePrice
-          */
-          public function testProductC(): void
-          {
-            $checkout = new App;
-  
-            // Check standard price
-            $checkout->addItem('C', 1);
-            $this->assertEquals('0.20',$checkout->getTotal());
-  
-            // Check first possible special price
-            $checkout->addItem('C', 1);
-            $this->assertEquals('0.38',$checkout->getTotal());
-           
-            // check second possible special price
-            $checkout->addItem('C', 1);
-            $this->assertEquals('0.50',$checkout->getTotal());
+		// Check adding another item to ensure standard price is added
+		$checkout->addItem('A', 1);
+		$this->assertEquals('1.80', $checkout->getTotal());
+	}
 
-            // Check that lowest possible special price is applied
-            $checkout->addItem('C', 1);
-            $this->assertEquals('0.70',$checkout->getTotal());
+	public function testProductB(): void
+	{
+		$checkout = new App;
 
-            // check another combo of special prices
-            $checkout->addItem('C', 1);
-            $this->assertEquals('0.88',$checkout->getTotal());
+		// Check standard price
+		$checkout->addItem('B', 1);
+		$this->assertEquals('0.30', $checkout->getTotal());
 
-            // check another combo of special prices
-            $checkout->addItem('C', 1);
-            $this->assertEquals('1.00',$checkout->getTotal());
-            
-          }
+		// Check discounted price
+		$checkout->addItem('B', 1);
+		$this->assertEquals('0.45', $checkout->getTotal());
 
-        /**
-          * @covers \App\CalculatePrice
-          */
-          public function testProductD(): void
-          {
-            $checkout = new App;
-  
-            // Check standard price
-            $checkout->addItem('D', 1);
-            $this->assertEquals('0.15',$checkout->getTotal());
-  
-            // Check special price
-            $checkout->addItem('A', 1);
-            $this->assertEquals('0.55',$checkout->getTotal());
-           
-            // Check that special price is only applicable once per paired item
-            $checkout->addItem('D', 1);
-            $this->assertEquals('0.70',$checkout->getTotal());
-  
-          }
+		// Check adding a third item
+		$checkout->addItem('B', 1);
+		$this->assertEquals('0.75', $checkout->getTotal());
+	}
 
-      /**
-       * @covers \App\CalculatePrice
-       */
-      public function testProductE(): void
-      {
-        $checkout = new App;
+	public function testProductC(): void
+	{
+		$checkout = new App;
 
-        $checkout->addItem('E', 1);
-        $this->assertEquals('0.05',$checkout->getTotal());
+		// Check standard price
+		$checkout->addItem('C', 1);
+		$this->assertEquals('0.20', $checkout->getTotal());
 
-        $checkout->addItem('E', 1);
-        $this->assertEquals('0.10',$checkout->getTotal());
+		// Check first possible special price
+		$checkout->addItem('C', 1);
+		$this->assertEquals('0.38', $checkout->getTotal());
 
-      }
-    }
+		// check second possible special price
+		$checkout->addItem('C', 1);
+		$this->assertEquals('0.50', $checkout->getTotal());
+
+		// Check that lowest possible special price is applied
+		$checkout->addItem('C', 1);
+		$this->assertEquals('0.70', $checkout->getTotal());
+
+		// check another combo of special prices
+		$checkout->addItem('C', 1);
+		$this->assertEquals('0.88', $checkout->getTotal());
+
+		// check another combo of special prices
+		$checkout->addItem('C', 1);
+		$this->assertEquals('1.00', $checkout->getTotal());
+	}
+
+	/**
+	 * @covers \App\addItem
+	 * @covers \App\getTotal
+	 */
+	public function testProductD(): void
+	{
+		$checkout = new App;
+
+		// Check standard price
+		$checkout->addItem('D', 1);
+		$this->assertEquals('0.15', $checkout->getTotal());
+
+		// Check special price
+		$checkout->addItem('A', 1);
+		$this->assertEquals('0.55', $checkout->getTotal());
+
+		// Check that special price is only applicable once per paired item
+		$checkout->addItem('D', 1);
+		$this->assertEquals('0.70', $checkout->getTotal());
+	}
+
+	/**
+	 * @covers \App\addItem
+	 * @covers \App\getTotal
+	 */
+	public function testProductE(): void
+	{
+		$checkout = new App;
+
+		// Check standard price
+		$checkout->addItem('E', 1);
+		$this->assertEquals('0.05', $checkout->getTotal());
+
+		// Check no discounts are applied
+		$checkout->addItem('E', 1);
+		$this->assertEquals('0.10', $checkout->getTotal());
+	}
+}
